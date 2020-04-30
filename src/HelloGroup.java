8c77183852d5520dc7d40fa0700e8f84a3c090e4
@selamawitzere <selamawitzere@gmail.com>
2020-04-29 22:16:02 -0400

    add HelloGroup

--- src/HelloGroup.java
+++ src/HelloGroup.java
@@ -5,7 +5,7 @@ public class HelloGroup {
         
         System.out.println("Hi. Everyone.This line is added by: RAHMAN");
                 
-        System.out.println("Hello , Test 1 GitHub");
+        System.out.println("Hello group-3pu , Test 1 GitHub ");
 
         
 
