6c31c6365c38943499b8701c89fd9a8d6870e250
e184651 <kandvirginia@gmail.com>
2020-04-29 21:35:24 -0400

    add HelloGroup.java

--- src/HelloGroup.java
+++ src/HelloGroup.java
@@ -4,6 +4,7 @@ public class HelloGroup {
         System.out.println("Hello Future Group-3 SDET");
         
         System.out.println("Hi. Everyone.This line is added by: RAHMAN");
+        System.out.println("Hello Everyone. This line added by: Amir");
 
     }
 }
