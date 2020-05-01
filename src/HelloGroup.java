52a3bd9194e6490f66dadc6ee30a17fc8523dc4e
Adilet Attokurov <64566842+Adilet121286@users.noreply.github.com>
2020-04-30 21:40:18 -0400

    Update HelloGroup.java

--- src/HelloGroup.java
+++ src/HelloGroup.java
@@ -11,6 +11,10 @@ public class HelloGroup {
         System.out.println("Hello group-3pu , Test 1 GitHub ");
         
         System.out.println("Great start! Go Group-3 SDET!"); 
+        
+        System.out.println("Hello java`s hunters. Nice start guys. Hope that our group will be the best"); 
+        
+        
 
         
    }
